63ff3cc8f4b704a9fb4e13d09d3bf32c05ca02c3
Tom Needham <needham.thomas@gmail.com>
2016-07-02 08:35:34 +0100

    Remove temp js file

--- temp.js
+++
@@ -1,7 +0,0 @@
-var ds18b20 = require('ds18b20');
-ds18b20.sensors(function(err, ids) {
-	console.log(ids);
-});
-
-// ... or sync call
-console.log('Current temperature is' + ds18b20.temperatureSync('10-00080283a977'));
